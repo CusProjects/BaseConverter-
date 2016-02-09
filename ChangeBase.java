88a635c30d19be6390250a75936f8f94ada88d94
JoseGibran <jpolonsky-2013382@kinal.edu.gt>
2016-02-09 16:51:11 -0600

    Convert Bin to Ter

--- ChangeBase.java
+++ ChangeBase.java
@@ -7,11 +7,9 @@ public class ChangeBase  {
 		do{
 			res = decimal % 3;
 			decimal = decimal / 3;
-			
 			result = String.valueOf(res) + result;
 			
-		}while(3 <= decimal);
-		result = String.valueOf(res) + result;
+		}while(decimal > 0);
 		return Integer.parseInt(result);
 	}
 	
@@ -31,8 +29,6 @@ public class ChangeBase  {
 	public static int toDecimal2(long n2) throws NumberFormatException{
 		char[] binaryArray = String.valueOf(n2).toCharArray();
 		double binaryCount = Math.pow(2, Double.parseDouble(String.valueOf(binaryArray.length))) / 2;
-		
-		
 		int decimal = 0;
 		for(char binary : binaryArray){
 			decimal += Integer.parseInt(String.valueOf(binary)) * binaryCount;
@@ -41,14 +37,32 @@ public class ChangeBase  {
 		return decimal;
 	}
 	
-	public static int toDecimal3(int n3) throws NumberFormatException{
-		return 1;
+	public static int toDecimal3(long n3) throws NumberFormatException{
+		char[] array = String.valueOf(n3).toCharArray();
+		double decimal = 0;
+		int pow = array.length - 1;
+		for(char n : array){
+			decimal += Integer.parseInt(String.valueOf(n)) * Math.pow(3, pow);
+			pow --;
+		}
+		return (int)decimal;
+	}
+	
+	public static int to2From3(long bin) {
+		return ChangeBase.toBase3(ChangeBase.toDecimal2(bin));
+	}
+	
+	public static int to3From2(long ter){
+		return ChangeBase.toBase2(ChangeBase.toDecimal3(ter));
 	}
 	
 	
-	
 	public static void main(String[] args){
+		//System.out.println(ChangeBase.toBase3(Integer.parseInt(args[0])));
 		//System.out.println(ChangeBase.toBase2(Integer.parseInt(args[0])));
-		System.out.println(ChangeBase.toDecimal2(Long.parseLong (args[0])));
+		//System.out.println(ChangeBase.toDecimal2(Long.parseLong (args[0])));
+		//System.out.println(toDecimal3(Long.parseLong(args[0])));
+		System.out.println(ChangeBase.to3From2(Long.parseLong(args[0]))); 
+		//System.out.println(ChangeBase.to2From3(Long.parseLong(args[0])));
 	}
 }
